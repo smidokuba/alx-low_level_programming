94ceb81cc1aa58fe713b00b12f2f0a6db6b3d74c
Chima Kabiri <smidokuba@gmail.com>
2023-07-16 07:31:34 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char n;
+	char w;
+
+	n = 'a';
+	w = 'A';
+	while
+		(n <= 'z') {
+			putchar(n);
+			n++;
+		}
+	while
+		(w <= 'Z') {
+			putchar(w);
+			w++;
+		}
+		putchar('\n');
+		return (0);
+}
