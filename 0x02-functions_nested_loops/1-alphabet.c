75ed296b77ffed2c6b7fb394f801ca4d435ca793
Chima Kabiri <smidokuba@gmail.com>
2023-07-19 19:24:49 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,21 @@
 #include "main.h"
+#include <unistd.h>
 
 /**
- * print_alphabet - print all alphabet in lowercase
+ * print_alphabet - Entry point
+ * Return:0
  */
 
 void print_alphabet(void)
 {
-	char letter;
+	char c = 'a';
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
 
-	_putchar('\n')
+	}
+	_putchar('\n');
+
 }
